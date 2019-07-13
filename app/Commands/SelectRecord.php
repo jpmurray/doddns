5911892a0850015f4c981887c1b0d989127d943d
Jean-Philippe Murray <jeanphilippe@pasunecompagnie.com>
2019-07-13 09:41:24 -0400

    Select and save a record to update in config

--- app/Commands/SelectRecord.php
+++ app/Commands/SelectRecord.php
@@ -2,39 +2,38 @@
 
 namespace App\Commands;
 
+use App\Helpers\ConfigHelper;
 use App\Helpers\DigitalOceanHelper;
-use Illuminate\Support\Facades\DB;
 use LaravelZero\Framework\Commands\Command;
 
-class AddRecord extends Command
+class SelectRecord extends Command
 {
     /**
      * The signature of the command.
      *
      * @var string
      */
-    protected $signature = 'records:add';
+    protected $signature = 'record:select';
 
     /**
      * The description of the command.
      *
      * @var string
      */
-    protected $description = 'Set which records to update.';
+    protected $description = 'Choose a record to update from a list of available records to update with current Digital Ocean token.';
 
     /**
      * Execute the console command.
      *
      * @return mixed
      */
-    public function handle(DigitalOceanHelper $digitalocean)
+    public function handle(DigitalOceanHelper $digitalocean, ConfigHelper $config)
     {
         $domains = $digitalocean->getDomains();
-
-        $selected_domain = $this->menu("Which domain?", $domains)->open();
+        $selected_domain = $this->menu("Which domain do you want to update a record on?", $domains)->open();
 
         if ($selected_domain === null) {
-            $this->info("Nothing selected. Exiting.");
+            $this->info("No domain selected. Nothing will be updated.");
             return;
         }
 
@@ -43,20 +42,20 @@ class AddRecord extends Command
         $records_for_menu = $records->mapWithKeys(function ($values, $key) {
             return [$key => "{$values->name} ({$values->type}): {$values->data}"];
         })->toArray();
-        
-        $selected_record = $this->menu("Which record?", $records_for_menu)->open();
+
+        $selected_record = $this->menu("Which record do you want updated?", $records_for_menu)->open();
 
         if ($selected_record === null) {
-            $this->info("Nothing selected. Exiting.");
+            $this->info("No record selected. Nothing will be updated.");
             return;
         }
 
-        DB::table('records')->insert(
-            ['domain' => $selected_domain,
+        $record = ['domain' => $selected_domain,
             'record_id' => $records[$selected_record]->id,
             'record_name' => $records[$selected_record]->name,
-            'record_type' => $records[$selected_record]->type]
-        );
+            'record_type' => $records[$selected_record]->type];
+
+        $config->set('record', $record);
 
         $this->info("Record {$records[$selected_record]->name} for domain {$selected_domain} will be updated on the next cycle.");
     }
